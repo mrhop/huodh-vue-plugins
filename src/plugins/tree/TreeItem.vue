b2976fac0db16f35e3c48630627a8bdcc80a864b
mrhop <233402923@qq.com>
2017-05-31 13:05:40 +0800

    add tree features:click

--- src/plugins/tree/TreeItem.vue
+++ src/plugins/tree/TreeItem.vue
@@ -1,7 +1,9 @@
 <template>
   <li class="tree-item">
     <a v-if="children" v-on:click="clickToggle"><span v-if="iconClass" :class="iconClass"/><span v-if="children">{{title}}</span></a>
-    <router-link v-else :to="url"><span v-if="iconClass" :class="iconClass"/>{{title}}</router-link>
+    <router-link v-else-if="url" :to="url"><span v-if="iconClass" :class="iconClass"/>{{title}}</router-link>
+    <a v-else-if="clickEvent" @click.prevent="clickEventLocal"><span v-if="iconClass" :class="iconClass"/>{{title}}</a>
+    <p v-else><span v-if="iconClass" :class="iconClass"/>{{title}}</p>
     <ul v-if="children" v-show="showChildren">
       <treeItem v-for="item in children" :itemData="item" :key="item.id"/>
     </ul>
@@ -14,9 +16,11 @@
       return {
         title: null,
         url: null,
+        clickEvent: null,
         children: null,
         iconClass: null,
-        showChildren: false
+        showChildren: false,
+        dataLocal: null
       }
     },
     created: function () {
@@ -28,8 +32,11 @@
         if (this.itemData) {
           this.title = this.itemData.title
           this.iconClass = this.itemData.iconClass
+          this.dataLocal = this.itemData.data
           if (!this.itemData.children) {
             this.url = this.itemData.url
+            /* eslint no-eval: 0 */
+            this.clickEvent = this.itemData.clickFunction ? eval(this.itemData.clickFunction) : (this.itemData.clickFunctionName ? eval(this.itemData.clickFunctionName) : null)
           } else {
             this.children = this.itemData.children
           }
@@ -37,6 +44,9 @@
       },
       clickToggle (event) {
         this.showChildren = !this.showChildren
+      },
+      clickEventLocal () {
+        this.clickEvent && this.clickEvent.apply(this)
       }
     }
   }
@@ -48,6 +58,9 @@
     > a {
       display: block;
       cursor: pointer;
+      margin-bottom: 3px;
+    }
+    > p {
       margin-bottom: 3px;
     }
     ul {
