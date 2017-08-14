dee96f1599fb80d6b4b4fa40461ab14fea076bcf
mrhop <233402923@qq.com>
2017-08-14 17:21:51 +0800

    bug fixed

--- src/plugins/formElement/FormElement.vue
+++ src/plugins/formElement/FormElement.vue
@@ -16,7 +16,10 @@
          v-else-if="options.type==='file'||options.type==='image'" v-for="n in (options.quantity||1)">
       <input type="file" class="form-control" v-on:change="fileChange" :name="options.name" :data-index="options.name+n"
              ref="formElementEl"/>
-      <a v-if="options.path&&options.path[n-1]" :href="options.path[n-1]" target="_blank">{{options.name+(options.quantity?n:'')}}</a>
+      <a v-if="options.type==='file'&&options.path&&options.path[n-1]" :href="options.path[n-1]" target="_blank">{{(options.label?options.label:options.name)+(options.quantity?n:'')}}</a>
+      <img :class="'img-'+options.name" v-else-if="options.type==='image'&&options.path&&options.path[n-1]"
+           :src="options.path[n-1]"
+           :title="(options.label?options.label:options.name)+(options.quantity?n:'')"/>
       <p v-if="options.validatedMsg&&options.validatedMsg[options.name+n]">{{options.validatedMsg[options.name+n]}}</p>
     </div>
     <input
